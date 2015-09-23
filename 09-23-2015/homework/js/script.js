a8037816cd8b6e114b99134806ddbafb2b2429f8
estebanaraya <portu12esteban@gmail.com>
2015-09-23 18:08:19 -0400

    homework 9/23

---
+++ 09-23-2015/homework/js/script.js
@@ -0,0 +1,9 @@
+$(".more1").click(function(){
+	$(".hide1").slideToggle('slow')
+});
+$(".more2").click(function(){
+	$(".hide2").slideToggle('slow')
+});
+$(".more3").click(function(){
+	$(".hide3").slideToggle('slow')
+});
